18a0b54b56a8132dd543e2dc1391f7cd879a769c
Sumit <sumitk.85@gmail.com>
2014-04-01 23:33:01 -0700

    Memcache set

--- PHP-MySql/MemcacheConnect.php
+++ PHP-MySql/MemcacheConnect.php
@@ -9,4 +9,11 @@
         return ($memcache) ? $memcache->get($key) : false;
     }
 
+    # Puts key / value pair into memcache ... called by mysql_query_cache()
+    function setCache($key,$object,$timeout = 60) {
+        global $memcache;
+        return ($memcache) ? $memcache->set($key,$object,MEMCACHE_COMPRESSED,$timeout) : false;
+    }
+
+
 ?>
