cdefb3f921c1342dfe5135250a4c7e80369c483a
Sumit <sumitk.85@gmail.com>
2014-04-13 22:59:35 -0700

    Finding the LRU key-val from cache and deleting it - initial checkin

---
+++ LRU_Cache.php
@@ -0,0 +1,65 @@
+<?php
+
+define ("MAX_LINES", 1000000);
+//$file = fopen('php://stdin', 'r');
+$file       = fopen('input00.txt', 'r');
+$totalCommands = fgets($file);
+$totalCommands = preg_replace('~[\r\n]+~', '', $totalCommands);
+
+
+if ($totalCommands > MAX_LINES) {
+    $totalCommands = MAX_LINES;
+}
+
+$cacheSize = 0;
+$cache     = array ();
+
+for ($i = 0; $i < $totalCommands; $i++) {
+    $key       = $value = 0;
+    $line      = fgets($file);
+    $line      = preg_replace('~[\r\n]+~', '', $line);
+    $lineArray = explode(" ", $line);
+
+    if (empty($lineArray[0])) {
+        continue;
+    }
+
+    $command = strtoupper($lineArray[0]);
+
+    $key = isset($lineArray[1]) ? trim($lineArray[1]) : "";
+    $key = (strlen($key) <= 10) ? $key : mb_substr($key, 0, 10);
+
+    $value = isset($lineArray[2]) ? trim($lineArray[2]) : "";
+    $value = (strlen($value) <= 10) ? $value : mb_substr($value, 0, 10);
+
+    if (($command != "BOUND") && ($command != "DUMP") && empty($cacheSize)) {
+        continue;
+    }
+
+    switch ($command) {
+
+        case "BOUND":
+            break;
+
+        case "SET":
+                
+            break;
+
+        case "GET":
+
+            break;
+
+        case "PEEK":
+
+            break;
+
+        case "DUMP":
+
+			break;
+
+        default:
+            break;
+
+    }
+
+}
