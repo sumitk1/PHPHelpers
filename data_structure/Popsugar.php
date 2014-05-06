37faa9412f690af090b66fbbed1271b0bff5c4de
Sumit <sumitk.85@gmail.com>
2014-05-06 00:38:48 -0700

    Cleanupt and doc blocks and code formatting

--- data_structure/Popsugar.php
+++ data_structure/Popsugar.php
@@ -1,128 +1,183 @@
 <?php
 
-class Node{
+class Node {
     public $data;
     public $link;
 
-    function __construct($data, $next = NULL){
+    function __construct($data, $next = NULL) {
         $this->data = $data;
         $this->link = $next;
     }
 }
 
-class CircularLinkedList{
+class CircularLinkedList {
     private $first;
     private $current;
     private $count;
 
-    function __construct(){
-        $this->count = 0;
-        $this->first = null;
-        $this->current = null;
+    function __construct() {
+        $this->count   = 0;
+        $this->first   = NULL;
+        $this->current = NULL;
     }
 
-    function isEmpty(){
+    /**
+     * This function returns if the list is empty or not
+     * @return bool - Is the list empty
+     */
+    function isEmpty() {
         return ($this->first == NULL);
     }
 
-    function pushLL($data){
-        $p = new Node($data);
-        if($this->isEmpty()){
-            $this->first = $p;
+    /**
+     * This function inserts the data in the Linked List
+     *
+     * @param $data - The item to be inserted in the Linked List
+     */
+    function pushInLinkedList($data) {
+        $newNode = new Node($data);
+        if ($this->isEmpty()) {
+            $this->first   = $newNode;
             $this->current = $this->first;
-        }
-        else{
-            $q = $this->first;
-            while($q->link != null)
-                $q = $q->link;
-            $q->link = $p;
+        } else {
+            $list = $this->first;
+            while ($list->link != NULL) {
+                $list = $list->link;
+            }
+            $list->link = $newNode;
         }
         $this->count++;
     }
 
-    function pushCLL($data){
+    /**
+     * This function inserts the element in the Circular Linked List
+     *
+     * @param $data - The item to be inserted in the Linked List
+     */
+    function pushInCircularLinkedList($data) {
 
-        if($this->isEmpty()) {
+        if ($this->isEmpty()) {
             $this->first   = new Node($data);
             $this->current = $this->first;
             $this->count++;
-        }
-        else {
+        } else {
             $this->current->link = new Node($data, $this->first);
-            $this->current = $this->current->link;
+            $this->current       = $this->current->link;
             $this->count++;
         }
     }
-    function find($value){
-        $q = $this->first;
-        $count = $this->count;
-        while($q->link != null || $count > 0){
-            if($q->data == $value)
-                $this->current = $q;
-            $q = $q->link;
-            $count--;
+
+    /**
+     * This function returns the position of the value searched in the list else False if not found
+     *
+     * @param $value - The value to search in the list
+     *
+     * @return bool
+     */
+    function find($value) {
+        $list     = $this->first;
+        $count    = $this->count;
+        $position = 1;
+        while ($list->link != NULL || $count > 0) {
+            if ($list->data == $value) {
+                $this->current = $list;
+
+                return $position;
+            } else {
+                $list = $list->link;
+                $count--;
+                $position++;
+            }
         }
-        return false;
+
+        return FALSE;
     }
 
-    function getNext(){
+    /**
+     * This function moves the current pointer to the next node and returns the data of that node
+     * @return int - The data of the Node element
+     */
+    function getNext() {
         $this->current = $this->current->link;
-        $result = $this->current->data;
+        $result        = $this->current->data;
+
         return $result;
     }
 
-    public function getFirst () {
+    /**
+     * This function returns the pointer to the first node in the List
+     * @return Node
+     */
+    public function getFirst() {
         return $this->first;
     }
 
-    public function getCurrent () {
+    /**
+     * This function returns the pointer to the current node in the List
+     * @return Node
+     */
+    public function getCurrent() {
         return $this->current;
     }
-    
-    public function printList () {
-        $q = $this->first;
+
+    /**
+     * This function prints the current state of the Circular linked list
+     * It shows 0 for chair empty and 1 as chair occupied
+     */
+    public function printList() {
+        $list  = $this->first;
         $count = $this->count;
-        while($count > 0){
-            echo " - " . $q->data . " - <br/>";
-            $q = $q->link;
+        while ($count > 0) {
+            echo " " . $list->data . " ";
+            $list = $list->link;
             $count--;
         }
     }
-    
-    public function findSurvivorPosition () {
-        $q = $this->first;
-        $count = $this->count;
-        $position = 1; 
-        while($count > 0){
-            if ($q->data) {
+
+    /**
+     * This function finds the first position of the chair which is not empty
+     *
+     * @return int - The position of the survivor
+     */
+    public function findSurvivorPosition() {
+        $list     = $this->first;
+        $count    = $this->count;
+        $position = 1;
+        while ($count > 0) {
+            if ($list->data) {
                 break;
             } else {
                 $position++;
             }
-            $q = $q->link;
+            $list = $list->link;
             $count--;
         }
+
         return $position;
     }
 }
 
+// New object of the CircularLinkedList
 $circularLinkedList = new CircularLinkedList();
 
-$count = 10;
-$skip  = 0;
-$d = 1;
-for($i = 0; $i < $count; $i++) {
-    $circularLinkedList->pushCLL($d);
+$count      = 100;
+$skip       = 0; // This is a flag that if set shows that we have already moved one person and we need to skip the current chair
+$isOccupied = 1;
+for ($i = 0; $i < $count; $i++) {
+    // Initializing all the elements of the Circular linked list to 1 - meaning all the chairs are occupied
+    $circularLinkedList->pushInCircularLinkedList($isOccupied);
 }
 
+// The current pointer after the above initialization points to the last node
+// The operation below makes it point to the first node
 $circularLinkedList->getNext();
 
-echo "<br/><br/>";
+echo "<br/>";
 
 while ($count != 1 && $count > 0) {
     $data = $circularLinkedList->getCurrent()->data;
     //echo "<br /> $count - $skip - $data<br />";
-    if ($count == 10) { // First node
+    if ($count == 100) { // First node
         $circularLinkedList->getCurrent()->data = "0";
         $circularLinkedList->getNext();
         $skip++;
@@ -143,15 +198,17 @@ while ($count != 1 && $count > 0) {
         continue;
     }
     if ($data == 0 && $count != 100) {
-        $circularLinkedList->getNext();   
+        $circularLinkedList->getNext();
         continue;
-     }
-     
-     if ($count == 1) break;
+    }
 
-}   
+    if ($count == 1) {
+        break;
+    }
+
+}
 
 $circularLinkedList->printList();
-echo "<br/>The position of the survivor is = " .$circularLinkedList->findSurvivorPosition(); 
+echo "<br/>The position of the survivor is = " . $circularLinkedList->findSurvivorPosition();
 
 ?>
